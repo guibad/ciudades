e0294767e192f2f5e14f33081c0c5b8a5d0d32ba
guibad <guillem.badenas@dxc.com>
2023-04-28 10:02:33 +0200

    Página NotFoundPage acabada

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React from 'react';
+import './NotFoundPage.css';
 
 export const NotFoundPage = () => {
   return (
-    <h2>404 NotFound</h2>
+    <div className="d-flex align-items-center justify-content-center error-container">
+      <div className="text-center">
+        <h1 className="display-1 fw-bold">404</h1>
+        <p className="fs-3"> <span className="text-danger">Oops!</span> Página no encontrada.</p>
+        <p className="lead">
+          La página que buscabas no existe.
+        </p>
+        <a href="/" className="btn btn-primary">Volver</a>
+      </div>
+    </div>
   )
 }

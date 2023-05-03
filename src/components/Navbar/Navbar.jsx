b7699a40c58f260e254b6f03eb50fd0e123da0d0
guibad <guillem.badenas@dxc.com>
2023-05-03 11:26:10 +0200

    NavBar simplificada y mejorada

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,31 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from "react-router-dom";
+import React from 'react';
+import { NavLink, useLocation } from "react-router-dom";
 import './Navbar.css';
 
 export const Navbar = () => {
-    const [activeLink, setActiveLink] = useState(null);
-    const navigate = useNavigate();
-
-    const refrescarLinkActivo = () => {
-        const pathname = window.location.pathname;
-        if (pathname === "/") setActiveLink("linkBuscar");
-        else if (pathname.includes("/buscar/")) setActiveLink("linkBuscar");
-        else if (pathname === "/historial") setActiveLink("linkHistorial");
-    }
-
-    const handleClick = (divId) => {
-        if (divId === "linkBuscar") {
-            navigate('/');
-        } else {
-            navigate('/historial');
-        }
-        setActiveLink(divId);
-        refrescarLinkActivo();
-    };
-
-    useEffect(() => {
-        refrescarLinkActivo();
-    }, [])
+    const location = useLocation();
 
     return (
         <nav className="navbar">
@@ -34,12 +12,8 @@ export const Navbar = () => {
             </a>
             <h2 className='titulo'>Ciudades</h2>
             <div className="links-container">
-                <div className={activeLink === "linkBuscar" ? "link_activo" : "link"} onClick={() => handleClick("linkBuscar")}>
-                    <h5>Buscar</h5>
-                </div>
-                <div className={activeLink === "linkHistorial" ? "link_activo" : "link"} onClick={() => handleClick("linkHistorial")}>
-                    <h5>Historial</h5>
-                </div>
+                <NavLink exact to="/" className={(link) => link.isActive || location.pathname.includes("buscar") ? "link_activo" : "link"}>Buscar</NavLink>
+                <NavLink to="/historial" className={(link) => link.isActive ? "link_activo" : "link"}>Historial</NavLink>
             </div>
         </nav>
     );

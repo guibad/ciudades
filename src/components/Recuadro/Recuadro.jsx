0a68348ee9447925ef725f597324aeadb2f0d103
guibad <guillem.badenas@dxc.com>
2023-05-03 09:34:25 +0200

    Cambiado boton minimizar

--- src/components/Recuadro/Recuadro.jsx
+++ src/components/Recuadro/Recuadro.jsx
@@ -13,7 +13,7 @@ export const Recuadro = (props) => {
             <div className="pestanya" style={{ height: minimizada ? '30px' : (props.altura || "200px") }}>
                 <div className="cabecera">
                     <span className="titulo-pestanya">{props.texto}</span>
-                    <button className="boton-minimizar" onClick={toggleMinimizada}>-</button>
+                    <button className="boton-minimizar" onClick={toggleMinimizada}> {minimizada ? <>&#9634;</> : "-"}</button>
                 </div>
                 {!minimizada && (
                     <div className="contenido">
